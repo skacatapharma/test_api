8fca792a0cee90c045469f9cb3177d690713a0f2
Akshada <akshadasuryawanshi18@gmail.com>
2023-02-20 15:32:57 +0530

    work

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const express = require('express');
 const {Client} = require('pg')
-
-const app = express();
-
 
 const client = new Client({
   host:"localhost",
@@ -11,18 +7,14 @@ const client = new Client({
   password:"cata@1234567",
   database:"CATAPHARMA_WEB"
 })
-  app.listen(4000, () =>{
-console.log("server is running on port 4000")
-  })
 
 client.connect();
 
-
-app.get('/products/:id', (req,res)=>{
-  client.query(`Select * from product_specifications where id=${req.params.id}`,(err,result)=>{
-    if(!err){
-      res.send(result.rows);
-    }
-  });
-  client.end;
+client.query(`Select * from product_specifications`,(err,res)=>{
+  if(!err){
+    console.log(res.rows);
+  } else {
+    console.log(err.message);
+  }
+   client.end;
 })
